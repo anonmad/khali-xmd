92937ef4e0d2864e84ea69e4f368d53ce65e510e
𝗔𝗡𝗢𝗡𝗠𝗔𝗗 𝗡𝗘𝗧𝗪𝗢𝗥𝗞 <anonymousmad74@gmail.com>
2026-03-26 14:10:59 +0300

    Update tool-vfc.js

--- plugins/tool-vfc.js
+++ plugins/tool-vfc.js
@@ -39,7 +39,7 @@ cmd({
         await conn.sendMessage(from, {
             document: fs.readFileSync(nmfilect), 
             mimetype: 'text/vcard', 
-            fileName: '𝚳𝐒𝚵𝐋𝚫-𝐂𝚮𝐔𝚰-𝚾𝚳𝐃.vcf', 
+            fileName: '𝐊𝐀𝐋𝐈-𝐌𝐃𝟏.vcf', 
             caption: `\nDone saving.\nGroup Name: *${cmiggc.subject}*\nContacts: *${cmiggc.participants.length}*\n> 📌 ᴘᴏᴡᴇʀ ʙʏ 𝚳𝐒𝚵𝐋𝚫-𝐂𝚮𝐔𝚰-𝚾𝚳𝐃`}, { quoted: mek });
 
         fs.unlinkSync(nmfilect); // Cleanup the file after sending
