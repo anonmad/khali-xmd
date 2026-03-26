9df1d8a062288c2933e90245419078ff4873b316
𝗔𝗡𝗢𝗡𝗠𝗔𝗗 𝗡𝗘𝗧𝗪𝗢𝗥𝗞 <anonymousmad74@gmail.com>
2026-03-26 14:09:31 +0300

    Update tool-trt.js

--- plugins/tool-trt.js
+++ plugins/tool-trt.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *𝚳𝐒𝚵𝐋𝚫-𝐂𝚮𝐔𝚰-𝚾𝚳𝐃-TRANSLATION*
+        const translationMessage = `> *𝐊𝐀𝐋𝐈-𝐌𝐃𝟏-TRANSLATION*
 
 > 🔤 *Original*: ${textToTranslate}
 
